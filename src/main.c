c5308b4517fa83c345622b566d9125ec7042a7f4
achitsaz <chitsaz.sci@gmail.com>
2025-01-25 13:05:45 -0500

    added the saveVTK func for checking the mesh file

--- src/main.c
+++ src/main.c
@@ -33,7 +33,42 @@ int main (void){
         if (open[i]==0) opencount++;
     }
     (opencount==0) ? printf("! this is open mesh.\n") : printf("* this is close mesh.\n");
-    
+    // creat mesh struct 
+    mesh *M1 = (mesh *)malloc(sizeof(mesh));
+    if (M1)
+    {
+        *M1 = (mesh){0}; // Set all integer and pointer fields to 0 or NULL
+    }
+    if (M1 == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed for M1 pointer\n");
+        exit(EXIT_FAILURE);
+    }
+    M1->elems =elems;M1->npoin =npoin;M1->nelem=nelem;M1->esure=esure;M1->esurp=esurp;M1->esurp_ptr=esurp_ptr;M1->nredge=Nredge;M1->open=open;M1->ptxyz=ptxyz;
+    #ifdef DEBUG
+           FunctionWithArgs prtelefield[] =
+            {
+                {"open", 1, nelem, open, SCA_int_VTK},
+            };
+        size_t countele = sizeof(prtelefield) / sizeof(prtelefield[0]);
+        FunctionWithArgs prtpntfield[] = {0};
+        size_t countpnt = 0;
+        CHECK_ERROR(SaveVTK("./", "checkmesh", 0, M1, tri3funcVTK, prtelefield, countele, prtpntfield, countpnt));
+    #endif
+    // // define sparse matrix of coefficient
+    // int *row_ptr = (int *)calloc((size_t)(nelem + 1), sizeof(int));
+    // int max_nnz = nelem * 6; // At most 6 non-zero entries per row
+    // double *val = (double *)malloc((size_t)max_nnz * sizeof(double));
+    // int *col_ind = (int *)malloc((size_t)max_nnz * sizeof(int));
+    // int nnz = 0;
+    // double *RHS = (double *)calloc((size_t)nelem, sizeof(double));
+    // if (row_ptr == NULL || val == NULL || col_ind == NULL || RHS == NULL)
+    // {
+    //     printf("Memory allocation failed.\n");
+    //     return 1;
+    // }
+
+
 
 
     // free dynamics arraies
@@ -43,5 +78,9 @@ int main (void){
     free (esure);
     free (esurp);
     free (esurp_ptr);
+    // free (row_ptr);
+    // free (val);
+    // free (col_ind);
+    // free (RHS);
     return 0; // success signal
 }
