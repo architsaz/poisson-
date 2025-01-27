d8abfb387b6d8e51c38e4725650ae3237f31b530
achitsaz <chitsaz.sci@gmail.com>
2025-01-27 15:59:02 -0500

    fixed libs problem

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
+// mesh funcs and mesh types
 #include "mesh.h"
-#include "common.h"
 #include "vector.h"
+// CRS lib and CG solver
+#include "CRSMat_types.h"
+#include "CRSmatfuncs.h"
+#include "CGSolver.h"
+#include "CGSolver_types.h"
+// General funcs
+#include "common.h"
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 int main (void){
     int npoin, nelem, *elems;
@@ -85,7 +94,7 @@ int main (void){
     #endif
     // define sparse matrix of coefficient
     int *row_ptr = (int *)calloc((size_t)(nelem + 1), sizeof(int));
-    int max_nnz = nelem * 6; // At most 6 non-zero entries per row
+    int max_nnz = nelem * (Nredge+1); // At most non-zero entries per row (element+neighbours)
     double *val = (double *)malloc((size_t)max_nnz * sizeof(double));
     int *col_ind = (int *)malloc((size_t)max_nnz * sizeof(int));
     int nnz = 0;
@@ -96,59 +105,89 @@ int main (void){
         return 1;
     }
 
-    // // calculate the symmetrical Positive Definite Coefficient martix and Right-Hand-Sided vector
-    // for (int ele = 0; ele < nelem; ele++)
-    // {
-    //     nnz++;
-    //     int IDele = nnz - 1;
-    //     // Known cells 
-    //     if (cell_stat[ele]<0){
-    //         // equation for this element is u=Tb
-    //         val[IDele] = 1;
-    //         col_ind[IDele] = ele;
-    //         RHS[ele] = (cell_stat[ele] == -1) ? 5 : 1; 
-    //     }else{
-    //     // Unknown cells 
-    //         for (int nei = 0; nei < nrpoin; nei++)
-    //         {
-    //             int neighbor = esure[nrpoin * ele + nei];
-    //             if (neighbor >= 0)
-    //             {   
-    //                 // Internal flux  
-    //                 // contribution for diagonal element of coefficient matrix 
-    //                 val[IDele] += 1;
-    //                 col_ind[IDele] = ele;
-    //                 // contribution for off-diagonal element of coefficient matrix 
-    //                 if (cell_stat[neighbor] <0 ){
-    //                     // between known and unknown cells
-    //                     RHS [ele] += (cell_stat[neighbor] == -1) ? 5 : 1;
-    //                 }else{
-    //                     // between 2known cells 
-    //                     val[nnz] = -1;
-    //                     col_ind[nnz] = neighbor;
-    //                     nnz++; // cause creat new element in the coefficient matrix
-    //                 }
+    // calculate the symmetrical Positive Definite Coefficient martix and Right-Hand-Sided vector
+    int order_in_cells [] = {0,1,1,2,2,0};
+    for (int ele = 0; ele < nelem; ele++)
+    {
+        nnz++;
+        int IDele = nnz - 1;
+        // Known cells 
+        if (cell_stat[ele]>0){
+            // equation for this element is u=Tb
+            val[IDele] = 1;
+            col_ind[IDele] = ele;
+            RHS[ele] = (cell_stat[ele] == 1) ? 1000 : 0; 
+        }else{
+        // Unknown cells 
+            for (int nei = 0; nei < Nredge; nei++)
+            {
+                int neighbor = esure[Nredge * ele + nei]; 
+                int lp1 = elems [Nredge * ele + order_in_cells[2*nei]]-1; 
+                int lp2 = elems [Nredge * ele + order_in_cells[2*nei+1]]-1; 
+                double dA = 0;
+                dA = SQUARE((ptxyz [3 * lp1 ] - ptxyz [3 * lp2 ] ));          // x-coordinate  
+                dA += SQUARE((ptxyz [3 * lp1 + 1] - ptxyz [3 * lp2 +1 ] ));   // y-coordinate
+                dA += SQUARE((ptxyz [3 * lp1 + 2] - ptxyz [3 * lp2 +2 ] ));   // z-coordinate
+                dA = sqrt (dA); 
+                double dl = 0;
+                dl = SQUARE(cen [Nredge * ele] - cen [Nredge *neighbor]);     // x-coordinate
+                dl += SQUARE(cen [Nredge * ele + 1] - cen [Nredge *neighbor + 1]);     // y-coordinate
+                dl += SQUARE(cen [Nredge * ele + 2] - cen [Nredge *neighbor + 2]);     // z-coordinate
+                dl = sqrt (dl);
+                double coef = dA/dl;
+                // Internal flux  
+                // contribution for diagonal element of coefficient matrix 
+                val[IDele] += coef;
+                col_ind[IDele] = ele;
+                // contribution for off-diagonal element of coefficient matrix 
+                if (cell_stat[neighbor] >0 ){
+                    // between known and unknown cells
+                    RHS [ele] += (cell_stat[neighbor] == 1) ? 1000 : 0;
+                }else{
+                    // between 2known cells 
+                    val[nnz] = -1 * coef;
+                    col_ind[nnz] = neighbor;
+                    nnz++; // cause creat new element in the coefficient matrix
+                }
+            }
+        }
+        row_ptr[ele + 1] = nnz;
+    }
+    row_ptr[nelem] = nnz;
 
-    //             }
-    //             else if (neighbor == -1)
-    //             { // Boundary condition
-    //             // coeff[nelem * ele + ele] += 2;               // Subtract halved contribution (distance halved)
-    //                 val[IDele] += 2;
-    //                 RHS[ele] += 5 * 2; // Add scaled boundary condition to RHS
-    //             }
-    //             else
-    //             {
-    //                 { // Boundary condition
-    //                 // coeff[nelem * ele + ele] += 2;               // Subtract halved contribution (distance halved)
-    //                     val[IDele] += 2;
-    //                     RHS[ele] += 1 * 2; // Add scaled boundary condition to RHS
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     row_ptr[ele + 1] = nnz;
-    // }
-    // row_ptr[nelem] = nnz;
+    // SOLVER SECTION
+    CRSMatrix A;
+    A.n = nelem;
+    A.nnz = nnz;
+    A.row_ptr = row_ptr;
+    A.col_index = col_ind;
+    A.values = val;
+
+    // unknown vector
+    double *u = (double *)calloc((size_t)A.n, sizeof(double));
+
+    // Solve using CG
+    clock_t start_time,end_time;
+    double cpu_time_used;
+    SolverConfig config = {100000, 1e-8, true};
+    solver_set_config(config);
+    // precond_conjugate_gradient(&A, RHS, u);
+    start_time = clock();
+    conjugate_gradient(&A, RHS, u);
+    end_time = clock();
+    cpu_time_used = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("* CG Solver execution time : %.2f seconds\n", cpu_time_used);
+
+    FunctionWithArgs prtelefield2[] =
+        {
+            {"open", 1, nelem, open, SCA_int_VTK},
+            {"BC", 1, nelem, cell_stat, SCA_int_VTK},
+            {"poisson", 1, nelem, u, SCA_double_VTK},
+        };
+    size_t countele2 = sizeof(prtelefield2) / sizeof(prtelefield2[0]);
+    FunctionWithArgs prtpntfield2[] = {0};
+    size_t countpnt2 = 0;
+    CHECK_ERROR(SaveVTK("./", "checkmesh", 0, M1, tri3funcVTK, prtelefield2, countele2, prtpntfield2, countpnt2));
 
 
 
@@ -168,5 +207,6 @@ int main (void){
     free (cen);
     free (M1);
     free (cell_stat);
+    free (u);
     return 0; // success signal
 }
